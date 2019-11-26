cfee64164b8508b358cd9f6af6c8507d8b1ee8e7
Zeyad Etman <zeyadetman@gmail.com>
2019-11-26 23:50:02 +0200

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useRef, useState } from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function defaultFormatTime(seconds) {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+  return [h, m > 9 ? m : h ? "0" + m : m || "0", s > 9 ? s : "0" + s]
+    .filter(a => a)
+    .join(":");
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default function Player({
+  audioSource,
+  decrementPeriod = 5,
+  incrementPeriod = 5,
+  onIncrement = () => {},
+  onDecrement = () => {},
+  onPlay = () => {},
+  onStop = () => {},
+  audioFunctions = {},
+  formatTime = time => {
+    return defaultFormatTime(time);
+  }
+}) {
+  const audio = useRef();
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  return (
+    <div>
+      <audio
+        preload="auto"
+        ref={audio}
+        onTimeUpdate={() => {
+          setCurrentTime(Math.floor(audio.current.currentTime));
+        }}
+        onLoadedMetadata={() => {
+          setDuration(Math.floor(audio.current.duration));
+          setCurrentTime(Math.floor(audio.current.currentTime));
+        }}
+        src={audioSource}
+        {...audioFunctions}
+      />
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            margin: "0 auto"
+          }}
+        >
+          <p style={{ margin: 0 }}>{formatTime(currentTime)}</p>
+          <p style={{ margin: 0 }}>{formatTime(duration)}</p>
+        </div>
+        <progress
+          style={{ width: "100%" }}
+          value={currentTime}
+          max={duration}
+        />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            audio.current.play();
+            onPlay();
+          }}
+        >
+          play
+        </button>
+        <button
+          onClick={() => {
+            audio.current.pause();
+            onStop();
+          }}
+        >
+          stop
+        </button>
+        <button
+          onClick={() => {
+            audio.current.currentTime += incrementPeriod;
+            onIncrement();
+          }}
+        >
+          {`+${incrementPeriod}`}
+        </button>
+        <button
+          onClick={() => {
+            audio.current.currentTime -= decrementPeriod;
+            onDecrement();
+          }}
+        >
+          {`-${decrementPeriod}`}
+        </button>
+      </div>
+    </div>
+  );
+}
